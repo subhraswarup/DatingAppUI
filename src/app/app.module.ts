95bd6b67b75171ab1937fc527fcb39736e3ea5e5
Subhra Swarup Behera <subhra.behera@tavant.com>
2019-02-05 21:31:24 +0530

    in between section 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,9 @@ import { UserService } from './_services/user.service';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
-import { MemberListResolver } from './_resolvers/member-list.resolver.1';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 
 
 export function tokenGetter() {
@@ -41,7 +43,8 @@ export function tokenGetter() {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MemberEditComponent
    ],
    imports: [
       BrowserModule,
@@ -67,7 +70,8 @@ export function tokenGetter() {
       AuthGuard,
       UserService,
       MemberDetailResolver,
-      MemberListResolver
+      MemberListResolver,
+      MemberEditResolver
    ],
    bootstrap: [
       AppComponent
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,7 +7,9 @@ import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
-import { MemberListResolver } from './_resolvers/member-list.resolver.1';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 
 
 export const appRoutes: Routes = [
@@ -20,6 +22,7 @@ export const appRoutes: Routes = [
         children: [
             { path: 'members', component : MemberListComponent, resolve: {users: MemberListResolver} },
             { path: 'members/:id', component : MemberDetailComponent, resolve : {user: MemberDetailResolver} },
+            { path: 'member/edit', component: MemberEditComponent, resolve : {user: MemberEditResolver}},
             { path: 'messages', component : MessagesComponent },
             { path: 'lists', component : ListsComponent },
         ]
